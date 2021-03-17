109b0305941001920c7227eb3b179785cfaaa2b8
santiago <santiagoagustinfernandez@gmail.com>
2021-03-17 16:30:14 -0300

    Adding Readme

--- kong-start.sh
+++ kong-start.sh
@@ -44,7 +44,7 @@ docker-compose up -d prometheus node_exporter
 
 echo "Prometheus running http://0.0.0.0:9090/"
 
-echo "Starting Splunk..."
+echo "Starting Graylog..."
 
 docker-compose up -d graylog elasticsearch mongo_graylog
 
