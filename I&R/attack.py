ca00b3c4aeaadf74095dd6d5b713ca88b3efaee9
Santiago Fernandez <santiagoagustinfernandez@gmail.com>
2021-03-24 13:22:45 -0300

    Last

--- I&R/attack.py
+++ I&R/attack.py
@@ -1,11 +1,21 @@
 import json
 import requests
-url = 'http://localhost:8000/frase/'
+import time
 
+url = 'http://api.local:8000/frase/'
 
-for i in range(30):
-    print(i)
-
-    r = requests.get(url + str(i))
-    files = r.json()
-    print(files)
+if __name__ == '__main__':
+    try:
+        main()
+        for i in range(100):
+            print("Request Numero %d " % i)
+            r = requests.get(url + str(i))
+            files = r.json()
+            print(files)
+            time.sleep(3)
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
